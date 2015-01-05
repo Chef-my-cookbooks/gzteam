8a1931f2966d3cb5cc8791fb929c926afe3e948f
Swaroop Kundeti <swaroop@axelerant.com>
2015-01-06 01:03:58 +0530

    fixed messed up directory structure

--- test/attributes/attributes/default.rb
+++
@@ -1,8 +0,0 @@
-#
-case os 
-when "debian", "ubuntu"
-	sublime[:binary] = "/usr/bin/subl"
-else
-	sublime[:binary] = "C:\Program Files\Sublime Text 2"
-end
-
--- test/attributes/default.rb
+++ test/attributes/default.rb
@@ -1,8 +1,1 @@
-#
-#case os 
-#when "debian", "ubuntu"
-#	test[:binary] = "/usr/bin/subl"
-#else
-#	test[:binary] = "C:\Program Files\Sublime Text 2"
-#end
 
