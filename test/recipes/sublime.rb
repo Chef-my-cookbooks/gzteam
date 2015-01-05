f74e1d71b6b5e56ecc4abac7e0bcfec666475f97
Swaroop Kundeti <swaroop@axelerant.com>
2015-01-06 00:57:30 +0530

    Made changes to sublime.rb for fixing windows path env var

--- test/recipes/sublime.rb
+++ test/recipes/sublime.rb
@@ -11,32 +11,16 @@
 # UBUNTU
 # Note: Since we are using PPA here, there is no necessary to symlink or export the PATH. Package manager will does this by default.
 
-if platform?("ubuntu")
-	bash "Sublime-text"  do
-		user "root"
-		code <<-EOH
-		  sudo dd-apt-repository ppa:webupd8team/sublime-text-2
-		  sudo apt-get update -y
-		EOH
-	end
-
-	package "sublime-text" do
-		name "sublime-text"
-		action :install
-	end
-end
-
-# DEBIAN.
-if platform?("debian")
+if platform?("debian","ubuntu")
 	bash "Sublime-text 2" do
 		user "root"
-		code <<-EOH
-		  wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.tar.bz2	
-		  tar -xvjf Sublime\ Text\ 2*.tar.bz2
-		  mv Sublime\ Text\ 2/ /opt/sublime-text-2/
-		  ln -s /opt/sublime-text-2 /usr/local/sublime-text-2
-		  ln -s /usr/local/sublime-text-2/sublime_text /usr/bin/subl
-		  rm Sublime\ Text\ 2*.tar.bz2
+		code <<<-EOH
+		wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.tar.bz2	
+		tar -xvjf Sublime\ Text\ 2*.tar.bz2
+		mv Sublime\ Text\ 2/ /opt/sublime-text-2/
+		ln -s /opt/sublime-text-2 /usr/local/sublime-text-2
+		ln -s /usr/local/sublime-text-2/sublime_text /usr/bin/subl
+		rm Sublime\ Text\ 2*.tar.bz2
 		EOH
 	end
 end
@@ -44,19 +28,21 @@ end
 
 # WINDOWS
 if platform?("windows")
-	windows_package "Sublime-text" do
-		architecture :i386
-		source "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20Setup.exe"
-		action :install
-	end
-
+	
 	windows_package "sublime-test 2" do
-		architecture :x86_64
 		source "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20x64%20Setup.exe"
 		action :install
 	end
 
-	windows_path "C:\Program Files\Sublime Text 2" do
-		action :add
-	end
+	ruby_block "chefdk_path_check" do
+  	  block do
+    	current_path = ENV['PATH'].split(';')
+   		sublime_path = 'C:\\Program Files\\Sublime Text 2\\sublime_text.exe'
+    	current_path.delete_if { |path| path == sublime_path }
+    	new_path = current_path.unshift(sublime_path)
+    	`Powershell -Command Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\\System\\CurrentControlSet\\Control\\Session Manager\\Environment' -Name PATH –Value '#{new_path}'`
+      end
+    end
 end
+
+	
